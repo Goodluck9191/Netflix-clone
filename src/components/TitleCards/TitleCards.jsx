f4110699b30f7737d59f986ea61e06b75b8888ac
goodlcuk <goodluckmasawe91@gmail.com>
2025-12-13 12:55:58 +0300

    Add the cards to the Home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+
+const TitleCards = ({title, category}) => {
+
+  const cardsRef = useRef(null);
+
+  const handWheel =(event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+
+  }
+
+
+
+  useEffect(()=>{
+    const node = cardsRef.current;
+    if (!node) return;
+    node.addEventListener('wheel', handWheel);
+    return () => {
+      node.removeEventListener('wheel', handWheel);
+    };
+  }, [])
+
+
   return (
     <div className='TitleCards'>
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className='card-list' ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
       
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_bunner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards.jsx'
 
 const Home = () => {
   return (
@@ -22,9 +23,15 @@ const Home = () => {
             <button className='btn'><img src={play_icon} alt="" />Play</button>
             <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
           </div>
+          <TitleCards />
         </div>
       </div>
-      
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"}/>
+        <TitleCards title={"Upcoming"}/>
+        <TitleCards title={"Top pics for you"}/>
+      </div>
     </div>
   )
 }
